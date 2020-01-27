406bc8c6946a0a340e216893e0f471a0b387c93d
Richard Hsu <kinghsusw@gmail.com>
2020-01-26 20:14:57 -0800

    visible enemy detection view

---
+++ Assets/Scripts/Enemies/DetectionScript.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionScript : MonoBehaviour
+{
+    [Header("Detection")]
+    [Range(0.0f, 179.0f)]
+    [SerializeField] float detectionConeAngle = 90.0f;
+    [SerializeField] float detectionRayDistance = 5.0f;
+    [SerializeField] int numberOfRaycasts = 2;
+
+    // ----- Private variables ----- 
+    // Detection cone variables
+    private Mesh mesh;
+    private float angleIncrement;
+    private Vector3 origin = Vector3.zero;
+    [SerializeField] private LayerMask layerMask;
+    private float startingAngle = 0f;
+
+    private void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        angleIncrement = detectionConeAngle / numberOfRaycasts;
+    }
+
+    private void Update()
+    {
+        origin = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Detect();
+    }
+
+    private void Detect()
+    {
+        // Initialize mesh arrays
+        Vector3[] vertices = new Vector3[numberOfRaycasts + 2];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[numberOfRaycasts * 3];
+
+        // Create the mesh
+        vertices[0] = transform.InverseTransformPoint(transform.position);
+
+        int vertexIndex   = 1;
+        int triangleIndex = 0; 
+        float angle = startingAngle; 
+        for (int i = 0; i <= numberOfRaycasts; i++)
+        {
+            Vector3 angledVector = GetVectorFromAngle(angle);
+            Vector3 vertex;
+            
+            RaycastHit2D raycastHit = DrawRaycast(origin, angledVector, detectionRayDistance, layerMask);
+            if (raycastHit.collider == null)
+            {
+                Vector3 worldVertex = origin + angledVector * detectionRayDistance;
+                vertex = transform.InverseTransformPoint(worldVertex);
+            }
+            else
+                vertex = transform.InverseTransformPoint(raycastHit.point);
+            
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrement;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+
+    // ----- PUBLIC SETTER FUNCTIONS ----- 
+    public void SetAimDirection(Vector3 aimDirection, bool isFacingLeft)
+    {
+        startingAngle = GetAngleFromVectorFloat(aimDirection) + detectionConeAngle / 2f;
+    }
+
+
+    // ----- HELPER FUNCTIONS -----
+    private RaycastHit2D DrawRaycast(Vector2 origin, Vector2 direction, float distance, LayerMask layerMask)
+    {
+        RaycastHit2D raycastHit = Physics2D.Raycast(origin, direction, distance, layerMask);
+        Debug.DrawRay(origin, direction.normalized * distance, Color.yellow);
+
+        return raycastHit;
+    }
+
+    private float DegreesToRad(float degree)
+    {
+        return degree * Mathf.PI / 180.0f;
+    }
+
+    private Vector3 GetVectorFromAngle(float angle)
+    {
+        float radian = DegreesToRad(angle);
+        return new Vector3(Mathf.Cos(radian), Mathf.Sin(radian));
+    }
+
+    private float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+
+
+    /* ----- CODE FROM PREVIOUS VERSION -----
+    private void SendRaycasts()
+    {
+        // Create a cone of detection
+        for (float i = -detectionConeAngle / 2; i <= detectionConeAngle / 2; i += angleIncrement)
+        {
+            Vector2 direction = CalcRayDir(i);
+            RaycastHit2D coneRaycast = DrawRaycast(transform.position, direction,
+                                                   detectionRayDistance);
+
+            if (coneRaycast.collider != false && coneRaycast.transform.tag == "Player")
+            {
+                Destroy(coneRaycast.transform.gameObject);
+
+                // TODO: implement checkpoint respawn
+            }
+        }
+    }
+
+    private Vector2 CalcRayDir(float angle)
+    {
+        // Determine whether to cast rays left or right
+        float baseDir;
+        if (isFacingLeft)
+            baseDir = Vector2.left.x;
+        else
+            baseDir = Vector2.right.x;
+
+        float radians = DegreesToRad(angle);
+        float height = CalcHeight(baseDir, radians);
+
+        Vector2 angledDir = new Vector2(baseDir, height);
+
+        return angledDir;
+    }
+
+    private float CalcHeight(float baseLen, float radian)
+    {
+        // FORMULA: tan(angle) = height / base
+        return Mathf.Tan(radian) * baseLen;
+    }
+    */
+}
---
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // ----- Public variables -----
+    [Header("Movement")]
+    [SerializeField] float speed = 8.0f;
+    [SerializeField] bool isFacingLeft = false;  // isFacingLeft is true if the AI is patrolling
+                                                 // in the left direction
+
+    [Header("AI")]
+    [SerializeField] float groundRayDistance = 2.0f;
+
+    // ----- Private variables -----
+    // GameObject component variables
+    private Rigidbody2D rb;
+    private Transform groundDetection;
+    private DetectionScript detectionScript;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundDetection = transform.Find("Ground Detection");
+        detectionScript = transform.Find("Ray Emitter").GetComponent<DetectionScript>();
+
+        if (isFacingLeft)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            speed *= -1;
+        }
+    }
+
+    private void Update()
+    {
+        /* ----- code to debug checkpoints -----
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<PlayerRespawn>().Respawn();
+        }
+        */
+
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        CheckGrounded();
+        Look();
+        Movement();
+    }
+
+    private void CheckGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.Raycast(groundDetection.position, Vector2.down, 
+                                                    groundRayDistance);
+
+        if (raycastHit.collider == false)
+        {
+            ChangeDirection();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Patrol changes direction when hitting a wall or 
+        // an obstacle.
+        if (collision.transform.tag == "Obstacle")
+        {
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        if (isFacingLeft)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        else
+            transform.eulerAngles = new Vector3(0, -180, 0);
+
+        speed *= -1;
+        isFacingLeft = !isFacingLeft;
+    }
+
+    private void Look()
+    {
+        // TODO: improve enemy detection here
+        if (isFacingLeft)
+            detectionScript.SetAimDirection(Vector3.left, true);
+        else
+            detectionScript.SetAimDirection(Vector3.right, false);
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+}
