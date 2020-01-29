a597ea4a5b3743c781e493b32792a0a7a1eceeac
Matthew Tran <39714315+SakanaMarket@users.noreply.github.com>
2020-01-28 17:07:45 -0800

    Obstacle object script for killing player

---
+++ Assets/Scripts/Obstacle_Death.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Death : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D obstacle)
+    {
+        // Attach this script to the Obstacle: Lasers / Electricity / etc
+        // This will destroy both the player
+        // and the instantiated obstacle (assuming the obstacle is instantiated)
+        
+        // Obstacle should have the tag "obstacle" (subjected to change)
+        if (gameObject.tag == "obstacle")
+        {
+            Destroy(gameObject);
+            // destroys the instantiated obstacle
+        }
+        // Player should have the tag "Player"
+        if (obstacle.tag == "Player")
+        {
+            Destroy(obstacle.gameObject);
+            // destroys the player object
+        }
+    }
+}
