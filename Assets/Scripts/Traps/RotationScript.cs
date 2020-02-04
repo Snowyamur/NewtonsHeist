10a95dc84e95c6e88c54166a193afbc25a64b961
Richard Hsu <kinghsusw@gmail.com>
2020-02-03 17:53:53 -0800

    more rotation stuff

--- Assets/Scripts/Traps/RotationScript.cs
+++ Assets/Scripts/Traps/RotationScript.cs
@@ -1,24 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RotationScript))]
+public class RotationEditor : Editor
+{
+    private SerializedProperty isCycle;
+    private SerializedProperty isClockwise;
+    private SerializedProperty startAngle;
+    private SerializedProperty endAngle;
+    private SerializedProperty rotationTime;
+
+    private void OnEnable()
+    {
+        isCycle = serializedObject.FindProperty("isCycle");
+        isClockwise = serializedObject.FindProperty("isClockwise");
+        startAngle = serializedObject.FindProperty("startAngle");
+        endAngle = serializedObject.FindProperty("endAngle");
+        rotationTime = serializedObject.FindProperty("rotationTime");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(isCycle, new GUIContent("Is Cycle"));
+        if (isCycle.boolValue)
+        {
+            EditorGUILayout.PropertyField(isClockwise, new GUIContent("Is Clockwise"));
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(startAngle, new GUIContent("Start Angle"));
+            EditorGUILayout.PropertyField(endAngle, new GUIContent("End Angle"));
+        }
+        EditorGUILayout.PropertyField(rotationTime, new GUIContent("Rotation Time"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
 
 // [ExecuteInEditMode]
+[System.Serializable]
 public class RotationScript : MonoBehaviour
 {
-    // [SerializeField] float angleOfRotation = 90f;
-    [SerializeField] float startAngle = 0f;
-    [SerializeField] float endAngle   = 90f;
-    [SerializeField] float rotationTime = 2f;
+    // Public variables
+    public bool isCycle = false;
+    public bool isClockwise = true;
+
+    public float startAngle = 0f;
+    public float endAngle   = 90f;
+    public float rotationTime = 2f;
+
+    // Private variables
+    private float angleToRotate;
+    private float rotationSpeed;
+
+    private void Start()
+    {
+        angleToRotate = endAngle - startAngle;
+        transform.rotation = Quaternion.Euler(0, 0, startAngle);
+
+        rotationSpeed = 360f / rotationTime;
+    }
 
     private void Update()
     {
-        Quaternion from = Quaternion.Euler(0, 0, endAngle);
-        Quaternion to = Quaternion.Euler(0, 0, startAngle);
-
-        float lerp = 0.5F * (1.0F + Mathf.Sin(Mathf.PI * Time.realtimeSinceStartup / rotationTime));
-        transform.localRotation = Quaternion.Lerp(from, to, lerp);
+        if (isCycle)
+            Spin();
+        else
+            PingPong();
     }
 
+    private void PingPong()
+    {
+        float lerp = 0.5f * (1.0F + Mathf.Sin(Mathf.PI * Time.realtimeSinceStartup / rotationTime));
+        float rotateFromStart = angleToRotate * lerp;
+
+        Quaternion target = Quaternion.Euler(0, 0, startAngle + rotateFromStart);
+        transform.rotation = target;
+    }
+
+    private void Spin()
+    {
+        if (isClockwise)
+            transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.deltaTime);
+        else
+            transform.RotateAround(transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+
+
+    // ----- INSPECTOR FUNCTIONS -----
     private void OnDrawGizmos()
     {
         DrawRotationPath();
