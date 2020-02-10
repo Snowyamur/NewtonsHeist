33f3b06a924f246ca813181f6472b6124cc2452a
Richard Hsu <kinghsusw@gmail.com>
2020-02-10 12:33:24 -0800

    portals

---
+++ Assets/Scripts/Portals/PortalManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    // ----- Private Variables ----- 
+    private GameObject player;
+    private Rigidbody2D playerRB;
+
+    private GameObject portal1;
+    private GameObject portal2;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRB = player.GetComponent<Rigidbody2D>();
+
+        portal1 = transform.GetChild(0).gameObject;
+        portal2 = transform.GetChild(1).gameObject;
+    }
+
+    public void TeleportPlayer(GameObject enterPortal)
+    {
+        GameObject exitPortal;
+        if (enterPortal == portal1)
+            exitPortal = portal2;
+        else
+            exitPortal = portal1;
+
+        // Retain player velocity after exiting the portal
+        Vector3 exitPortalNormal = exitPortal.GetComponent<PortalScript>().GetPortalNormal();
+        Vector3 exitVelocity = exitPortalNormal * playerRB.velocity.magnitude;
+        playerRB.velocity = exitVelocity;
+
+        //set the player position to just in front of the portal.
+        player.transform.position = exitPortal.transform.position + exitPortalNormal * 3;
+    }
+}
---
+++ Assets/Scripts/Portals/PortalScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    [SerializeField] private bool enterFromLeft = true;
+    
+    private Vector3 portalNormal;
+    private PortalManager managerScript;
+
+    private void Start()
+    {
+        if (enterFromLeft)
+            portalNormal = -transform.right;
+        else
+            portalNormal = transform.right;
+
+        managerScript = transform.parent.GetComponent<PortalManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            managerScript.TeleportPlayer(gameObject);
+    }
+
+    public Vector3 GetPortalNormal()
+    {
+        return portalNormal;
+    }
+}
