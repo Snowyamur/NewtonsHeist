3a24cfe7064604166e4b5ec65ff5de46e1dd315d
Matthew Tran <haqt@uci.edu>
2020-02-17 20:03:28 -0800

    Timers
    Added count up/down timers

---
+++ Assets/Scripts/Timer/Count_down_Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count_down_Timer : MonoBehaviour
+{
+    public Text text_time;
+    public float hours;
+    public float minutes;
+    public float seconds;
+    public float time;
+    private float speed = 1;
+    void Start()
+    {
+        text_time = GetComponent<Text>();
+        time = (hours * 3600) + (minutes * 60) + seconds;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (time <= 0)
+        {
+            time = 0f;
+            text_time.text = "00:00:00:00";
+        }
+        else
+        {
+            time -= Time.deltaTime;
+            string hr = Mathf.Floor((time % 216000) / 3600).ToString("00");
+            string min = Mathf.Floor((time % 3600) / 60).ToString("00");
+            string sec = (time % 60).ToString("00.00");
+            text_time.text = hr + ":" + min + ":" + sec;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Timer/Count_up_Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count_up_Timer : MonoBehaviour
+{
+    public Text text_time;
+    public float time;
+    private float speed = 1;
+    void Start()
+    {
+        text_time = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        string hours = Mathf.Floor((time % 216000) / 3600).ToString("00");
+        string minutes = Mathf.Floor((time % 3600) / 60).ToString("00");
+        string seconds = (time % 60).ToString("00.00");
+        //string milliseconds = ((time * 1000)%1000).ToString("f2");
+        text_time.text = hours + ":" + minutes + ":" + seconds;
+    }
+}
