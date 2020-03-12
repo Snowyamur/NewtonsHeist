b1e0f7a59cb0e8d0539c6be1bc19f9e87092dc74
Wazyre <32106559+Wazyre@users.noreply.github.com>
2020-03-12 05:23:35 -0700

    Add pause menu buttons to menu

--- Assets/Scripts/PausingScript.cs
+++ Assets/Scripts/PausingScript.cs
@@ -10,7 +10,7 @@ public class PausingScript : MonoBehaviour
 
     void Awake()
     {
-        pauseMenu = transform.GetChild(1).gameObject;
+        pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
         pauseMenu.SetActive(false); // now that we found the pausemenu, make it inactive.
     }
 
---
+++ Assets/Scripts/SceneDatas/Level3.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Holds a scene's data
+[System.Serializable]
+public class Level3 : SceneData
+{
+    //Scene name to transfer to
+    string scene = "Level 3";
+
+    //Player's beginning position in new scene
+    float m_xPos = -14f;
+    float m_yPos = 15f;
+
+    //Scene's boundaries
+    float m_maxX = 55f;
+    float m_minX = -8.5f;
+    float m_maxY = 9.5f;
+    float m_minY = -53f;
+
+    public string getScene() {return scene;}
+
+    public override float xPos
+    {
+      get{return m_xPos;}
+    }
+    public override float yPos
+    {
+      get{return m_yPos;}
+    }
+    public override float maxX
+    {
+      get{return m_maxX;}
+    }
+    public override float minX
+    {
+      get{return m_minX;}
+    }
+    public override float maxY
+    {
+      get{return m_maxY;}
+    }
+    public override float minY
+    {
+      get{return m_minY;}
+    }
+
+}
