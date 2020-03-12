e0d27c32a368678c02b800e25f1d06cdd64a5153
Wazyre <32106559+Wazyre@users.noreply.github.com>
2020-03-12 07:12:44 -0700

    Add particles to checkpoints

--- Assets/Scripts/Checkpoints/ChangeSprite.cs
+++ Assets/Scripts/Checkpoints/ChangeSprite.cs
@@ -6,19 +6,23 @@ using UnityEngine.UI;
 public class ChangeSprite : MonoBehaviour
 {
     SpriteRenderer checkSprite;
+    ParticleSystem ps;
 
     public Sprite newSprite;
 
     void Start()
     {
         checkSprite = GetComponent<SpriteRenderer>();
+        ps = GetComponent<ParticleSystem>();
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.transform.tag == "Player")
         {
+            ParticleSystem.MainModule psMain = ps.main;
             checkSprite.sprite = newSprite;
+            psMain.startColor = new Color(0.078f, 0.99f, 0.9f, 1f);
         }
     }
 }
